a7b036e63664e710b75cf4f7b6a79bf900ee49da
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2019-05-14 13:34:47 -0400

    correction bug links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,13 @@
         @isset($user)
             <h2 class="text-title mb-3">{{ __('Photos de ') . $user->name }}</h2>
         @endif
-
+<<<<<<< HEAD
+        
+=======
+        <div class="d-flex justify-content-center mb-3">
+            {{ $images->links() }}
+        </div>
+>>>>>>> 96833d438309d608894cb96600806a64d7b85805
         <div class="card-columns">
             @foreach($images as $image)
                 <div class="card" id="image{{ $image->id }}">
@@ -43,7 +49,7 @@
                         <button type="submit">Sigaler l'image</button>
 
                            </form>
-                           @include('partials.delete')
+
                         </div>
                         
                     </div>
