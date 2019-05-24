014b35645ec13a488c2874d53cc5fbaa710dac26
PatBoies <patboies@hotmail.com>
2019-05-24 10:19:53 -0400

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 @section('content')
 <main class="container">
-   
+
     @isset($location)
     <h2 class="text-title mb-3">{{ $location->name }}</h2>
     @endif
     @isset($user)
     <h2 class="text-title mb-3">{{ __('Photos de ') . $user->name }}</h2>
     @endif
-   
+
     <div class="card-columns">
         @foreach($images as $image)
         <div class="card" id="image{{ $image->id }}">
@@ -20,24 +20,28 @@
                     {{ $image->location->name }}
                 </div>
                 <em>
-                    <a href="{{ url('user/' . $image->user_id . '/images') }}" data-toggle="tooltip" title="{{ __('Voir les photos de ') . $image->user->name }}">{{ $image->user->name }}</a>
+                    <a href="{{ url('user/' . $image->user_id . '/images') }}" data-toggle="tooltip"
+                        title="{{ __('Voir les photos de ') . $image->user->name }}">{{ $image->user->name }}</a>
                 </em>
                 <div class="pull-right">
                     <em>
                         {{ $image->created_at->formatLocalized('%x') }}
                     </em>
                 </div>
-                <div class="pull-right">
+                <div class="row">
                     <form class="flag" method="POST" action="{{ url('imagesFlag/'.$image->id) }}">
                         @csrf
                         {{ method_field('PUT') }}
-                        <button class="btn btn-warning mt-3" type="submit" data-toggle="tooltip" title="@lang('Signaler cette image')">
-                        <i class="fas fa-flag fa-lg"></i>
+                        <button class="btn btn-warning mt-3" type="submit" data-toggle="tooltip"
+                            title="@lang('Signaler cette image')">
+                            <i class="fas fa-flag fa-lg"></i>
                         </button>
                     </form>
+                    <div>
                     @if ($image->user_id === Auth::user()->id or Auth::user()->role == "admin")
-                        @include('partials.delete')
-                    @endif    
+                    @include('partials.delete')
+                    @endif
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,7 +55,6 @@
 
 @section('script')
 <script>
-   
     // custom lightbox
     $(() => {
         $('[data-toggle="tooltip"]').tooltip()
@@ -59,14 +62,14 @@
             delegate: 'a.image-link',
             type: 'image',
             tClose: '@lang("Fermer (Esc)")'
-            @if($images-> count() > 1),
+            @if($images - > count() > 1),
             gallery: {
                 enabled: true,
                 tPrev: '@lang("Précédent (Flèche gauche)")',
                 tNext: '@lang("Suivant (Flèche droite)")'
             },
             callbacks: {
-                buildControls: function() {
+                buildControls: function () {
                     this.contentContainer.append(this.arrowLeft.add(this.arrowRight))
                 }
             }
@@ -75,4 +78,4 @@
     });
 
 </script>
-@endsection    
+@endsection
