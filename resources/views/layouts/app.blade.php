8c59579e0f44d7fb1f56d80cf248678550786c27
gabrielnadoncanada <gabrielnadoncanada@gmail.com>
2019-05-24 09:57:00 -0400

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,11 +96,10 @@
                         </div>
                     </li>
                 </ul>
-
-            </div>
-            @include('components.form-search')   
+                @include('components.form-search')   
 @endauth
 @endif
+            </div>
         </nav>
         <main class="pt-5 mt-5">
             <div class="container">
